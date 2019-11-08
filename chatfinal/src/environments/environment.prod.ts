5d9b66886817dc65dcb6f1e5de52bc58e2872ddb
Aniket Ghorpade <48329149+Anigor66@users.noreply.github.com>
2019-11-09 00:20:16 +0530

    Update environment.prod.ts

--- chatfinal/src/environments/environment.prod.ts
+++ chatfinal/src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyCPECVGU-3AqwzWtimWBjwnFpAOCRoo_bo',
-    authDomain: 'chat-workwiz.firebaseapp.com',
-    databaseURL: 'https://chat-workwiz.firebaseio.com',
-    projectId: 'chat-workwiz',
-    storageBucket: 'chat-workwiz.appspot.com',
-    messagingSenderId: '834313635151',
-    appId: '1:834313635151:web:e710dea555a0a6185fb13a',
-    measurementId: 'G-ETJF4WRN5V'
-  }
 };
